c1d8345312f3e65c5be3b2aa688343fc489efb41
Daniel Wulff <danielwulff.ee@gmail.com>
2023-12-01 13:16:47 +0000

    part two complete

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,23 +1,90 @@
 function day1(input) {
   const lines = input.trim().split('\n');
-  const sum = lines.reduce((accum, current) => {
-    return accum + calibrationLineValue(current);
+
+  const sumPartOne = lines.reduce((accum, current) => {
+    return accum + calibrationLineValuePartOne(current);
+  }, 0);
+
+  const sumPartTwo = lines.reduce((accum, current) => {
+    return accum + calibrationLineValuePartTwo(current);
   }, 0);
 
   return {
-    partOne: sum,
-    partTwo: null,
+    partOne: sumPartOne,
+    partTwo: sumPartTwo,
   };
 }
 
-function calibrationLineValue(input) {
+function calibrationLineValuePartOne(input) {
   const charArray = Array.from(input);
-  const first = charArray.find(c => !isNaN(c));
-  const last = charArray.findLast(c => !isNaN(c));
+  const first = charArray.find(findDigit);
+  const last = charArray.findLast(findDigit);
   return Number(first + last);
+}
+
+function findDigit(c) {
+  return !isNaN(c);
+}
+
+const findStringDigitExp = /(\d|one|two|three|four|five|six|seven|eight|nine)/;
+const findReversedStringDigitExp = /(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)/;
+
+function calibrationLineValuePartTwo(input) {
+  const reversedInput = input.split('').reverse().join('');
+  const first = convertToStringDigit(findStringDigitExp.exec(input)[1]);
+  const last = convertToStringDigit(findReversedStringDigitExp.exec(reversedInput)[1]);
+  return Number(first + last);
+}
+
+function convertToStringDigit(string) {
+  let number = Number(string);
+  if (isNaN(number)) {
+    switch (string) {
+      case 'one':
+      case 'eno':
+        number = '1';
+        break;
+      case 'two':
+      case 'owt':
+        number = '2';
+        break;
+      case 'three':
+      case 'eerht':
+        number = '3';
+        break;
+      case 'four':
+      case 'ruof':
+        number = '4';
+        break;
+      case 'five':
+      case 'evif':
+        number = '5';
+        break;
+      case 'six':
+      case 'xis':
+        number = '6';
+        break;
+      case 'seven':
+      case 'neves':
+        number = '7';
+        break;
+      case 'eight':
+      case 'thgie':
+        number = '8';
+        break;
+      case 'nine':
+      case 'enin':
+        number = '9';
+        break;
+    }
+    return number;
+  } else {
+    return string;
+  }
 }
 
 module.exports = {
   day1,
-  calibrationLineValue,
+  calibrationLineValuePartOne,
+  calibrationLineValuePartTwo,
 };
--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -1,19 +1,38 @@
-const { day1, calibrationLineValue } = require("./day1");
+const { day1, calibrationLineValuePartOne, calibrationLineValuePartTwo } = require("./day1");
 
-let input = `1abc2
+let inputPartOne = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
 
-test("calibrationLineValue", () => {
-  expect(calibrationLineValue('1abc2')).toBe(12);
-  expect(calibrationLineValue('pqr3stu8vwx')).toBe(38);
-  expect(calibrationLineValue('a1b2c3d4e5f')).toBe(15);
-  expect(calibrationLineValue('treb7uchet')).toBe(77);
+let inputPartTwo = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
+test("calibrationLineValuePartOne", () => {
+  expect(calibrationLineValuePartOne('1abc2')).toBe(12);
+  expect(calibrationLineValuePartOne('pqr3stu8vwx')).toBe(38);
+  expect(calibrationLineValuePartOne('a1b2c3d4e5f')).toBe(15);
+  expect(calibrationLineValuePartOne('treb7uchet')).toBe(77);
+});
+
+test("calibrationLineValuePartTwo", () => {
+  expect(calibrationLineValuePartTwo('two1nine')).toBe(29);
+  expect(calibrationLineValuePartTwo('eightwothree')).toBe(83);
+  expect(calibrationLineValuePartTwo('abcone2threexyz')).toBe(13);
+  expect(calibrationLineValuePartTwo('xtwone3four')).toBe(24);
+  expect(calibrationLineValuePartTwo('4nineeightseven2')).toBe(42);
+  expect(calibrationLineValuePartTwo('zoneight234')).toBe(14);
+  expect(calibrationLineValuePartTwo('7pqrstsixteen')).toBe(76);
 });
 
 test("day1", () => {
-  const result = day1(input);
-  expect(result.partOne).toBe(142);
-  expect(result.partTwo).toBe(null);
+  const { partOne } = day1(inputPartOne);
+  const { partTwo } = day1(inputPartTwo);
+  expect(partOne).toBe(142);
+  expect(partTwo).toBe(281);
 });
