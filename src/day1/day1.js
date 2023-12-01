46f562cfc1c146db478bc7bac659346d86514bab
Daniel Wulff <danielwulff.ee@gmail.com>
2023-12-01 11:48:58 +0000

    part1 complete

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,11 +1,23 @@
 function day1(input) {
+  const lines = input.trim().split('\n');
+  const sum = lines.reduce((accum, current) => {
+    return accum + calibrationLineValue(current);
+  }, 0);
+
   return {
-    partOne: null,
+    partOne: sum,
     partTwo: null,
   };
 }
 
+function calibrationLineValue(input) {
+  const charArray = Array.from(input);
+  const first = charArray.find(c => !isNaN(c));
+  const last = charArray.findLast(c => !isNaN(c));
+  return Number(first + last);
+}
+
 module.exports = {
   day1,
-  // Note: you can add helper functions for testing within .test.js here if you wish
+  calibrationLineValue,
 };
--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -1,13 +1,19 @@
-const { day1 } = require("./day1");
+const { day1, calibrationLineValue } = require("./day1");
 
-let input = `replace me
-some input string to mimic
-the input files
+let input = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`;
 
-`;
+test("calibrationLineValue", () => {
+  expect(calibrationLineValue('1abc2')).toBe(12);
+  expect(calibrationLineValue('pqr3stu8vwx')).toBe(38);
+  expect(calibrationLineValue('a1b2c3d4e5f')).toBe(15);
+  expect(calibrationLineValue('treb7uchet')).toBe(77);
+});
 
 test("day1", () => {
   const result = day1(input);
-  expect(result.partOne).toBe(null);
+  expect(result.partOne).toBe(142);
   expect(result.partTwo).toBe(null);
 });
